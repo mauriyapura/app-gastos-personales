7f1dbaa1d7279756eb0d767ff1bf63dfa0233fce
Mauricio <mauri.yapura@yahoo.com>
2021-10-19 19:50:39 -0300

    navbar responsive

--- client/src/components/ui/NavBar.js
+++ client/src/components/ui/NavBar.js
@@ -9,14 +9,12 @@ export const Navbar = () => {
     const history = useHistory();    
 
     const handleLogout = ()=>{
-
         history.replace("/login");
         dispatch({
             type: types.logout
         });
     }
     const handleSignIn = ()=>{
-
         history.replace("/signin");
         dispatch({
             type: types.signin
@@ -24,39 +22,48 @@ export const Navbar = () => {
         console.log("Entrando a Sign In")
     }
     const handleLogin = ()=>{
-
-        history.replace("/login");       
-        
+        history.replace("/login");    
     }
 
 
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">            
-            <div className="ms-4 title">
-                APLICACION DE GASTOS
-            </div>                                  
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div className="container">           
+                
+                <a href="#" className="navbar-brand">APP DE GASTOS</a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#menu"
+                    aria-controls="menu"
+                    aria-expanded="false"
+                    aria-label="Mostrar / Ocultar Menu"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>                                  
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                <ul className="navbar-nav ms-auto me-4">           
+                <div className="collapse navbar-collapse" id="menu">
+                    <ul className="navbar-nav ms-auto me-4 mb-2">           
                                         
-                    { 
-                        (logged)
-                            ? ( 
-                                <>
-                                <span className="nav-item nav-link text-info">{email}</span>
-                                <button className="nav-item nav-link btn" onClick={handleLogout}>Logout</button>
-                                </>
-                              ) 
-                            : ( 
-                                <>
-                                <button className="nav-item nav-link btn" onClick={handleLogin}>LogIn</button>
-                                <button className="nav-item nav-link btn" onClick={handleSignIn}>SignIn</button>
-                                </>
-                              ) 
-                    }                 
-                    
-                </ul>
-            </div>
+                        { 
+                            (logged)
+                                ? ( 
+                                    <>
+                                    <span className="nav-item nav-link text-info">{email}</span>
+                                    <button className="nav-item nav-link btn" onClick={handleLogout}>Logout</button>
+                                    </>
+                                ) 
+                                : ( 
+                                    <>
+                                    <button className="nav-item nav-link btn" onClick={handleLogin}>LogIn</button>
+                                    <button className="nav-item nav-link btn" onClick={handleSignIn}>SignIn</button>
+                                    </>
+                                ) 
+                        }             
+                    </ul>
+                </div>
+            </div>            
         </nav>
     )
 }
